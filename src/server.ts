277d79aae22eb62669193ac44d4c781bae857e5b
hoonboon <hongwen77@gmail.com>
2018-06-02 17:55:05 +0800

    Enable concurrency

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import * as express from "express";
 import log from "./log";
 
-const app = express();
+const throng = require("throng");
 
-app.get("/", (req, res) => {
-    res.json(JSON.stringify({ok: 1})).end();
-});
+// heroku: user to set env variable WEB_MEMORY, heroku will calculate and set the WEB_CONCURRENCY
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
+const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT || 5000, () => {
-    log.info("app running");
+function start(id: number) {
+    const app = express();
+
+    app.get("/", (req, res) => {
+        res.json(JSON.stringify({ok: 1})).end();
+    });
+
+    app.listen(PORT, () => {
+        log.info(`worker ${id} running on ${PORT}`);
+    });
+}
+
+throng({
+    workers: WORKERS,
+    lifetime: Infinity,
+    start: start
 });
